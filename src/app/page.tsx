3bed3774ee7747b525be2ba3ad006575c05f529a
donovanduyho <donovanduyho@gmail.com>
2024-09-03 13:36:11 -0400

    test db connections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,36 @@
-import Link from "next/link";
+import { db } from "~/server/db";
+import { postsTable, usersTable } from "~/server/db/schema";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const post = await db.query.postsTable.findMany();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <>
+      <form
+        action={async () => {
+          "use server";
+          await db.insert(usersTable).values({
+            name: "Test",
+            age: 20,
+            email: "test@test.com",
+            id: 1,
+          });
+
+          await db.insert(postsTable).values({
+            title: "Test",
+            content: "This is a test.",
+            userId: 1,
+          });
+        }}
+      >
+        <button>Submit</button>
+      </form>
+
+      {post.map((post) => (
+        <div key={post.id}>
+          <p>{post.content}</p>
         </div>
-      </div>
-    </main>
+      ))}
+    </>
   );
 }
